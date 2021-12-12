d40fdbabf838be503a1cc70c325dbe5c59bfa658
FranckyCastell <francesc.castell8@gmail.com>
2021-12-12 19:51:01 +0100

    All Ready

--- HomeApp/views.py
+++ HomeApp/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from HomeApp.models import Users
-from CoursesApp.models import Categories
 
 # Create your views here.
 
@@ -8,9 +6,6 @@ from CoursesApp.models import Categories
 def home(request):
     url = 'HOME'
     description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, cumque.'
-
-    categories = Categories.objects.all()
-    users = Users.objects.all()
 
     context = {'url': url, 'description': description}
     return render(request, 'HomeApp/home.html', context)
